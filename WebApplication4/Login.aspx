c298ea9fd768214de333000d643f4310c4a4ef66
barrygleeson <barry@retailit.ie>
2014-04-02 22:15:11 +0100

    First Upload

---
+++ WebApplication4/Login.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Login.aspx.vb" Inherits="WebApplication4.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="height: 188px">
+    
+        login<br />
+        <asp:TextBox ID="TextBox1" runat="server" Height="26px"></asp:TextBox>
+        <br />
+        Password<br />
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Login" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+    
+    </div>
+    <p>
+        &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ WebApplication4/Login.aspx.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Data.SqlClient
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    '.Net can be reverse enginered and as such connection strings in code are inadviseable
+    Dim DS1 As String = "Data Source=" + "barry-hp" +
+     ";Initial Catalog=SecDemo;Integrated Security=True"
+
+
+    Dim QRY1 As String = "select  distinct 'true' from users"
+
+
+    Public Function SQLlogin(ByVal username As String, ByVal password As String)
+        Dim dt1 As DataTable
+        dt1 = New DataTable
+        dt1.Clear()
+        Dim rowcount As Integer = 0
+
+        'Creating a string with variables to pass through to the sql connection is a bad idea and open to SQL injection
+
+        Using da1 As New SqlDataAdapter(QRY1 + " where username ='" + username + "' and password ='" + password + "'", DS1)
+            da1.Fill(dt1)
+            rowcount = dt1.Rows.Count
+        End Using
+        Return rowcount
+    End Function
+
+
+
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        Dim response As Integer = 0
+        response = SQLlogin(Me.TextBox1.Text, Me.TextBox2.Text)
+        If response.ToString = 0 Then
+            Label1.Text = "You are not logged in"
+        End If
+
+        If response.ToString = 1 Then
+            Label1.Text = "You are logged in"
+        End If
+
+    End Sub
+
+
+
+
+End Class
